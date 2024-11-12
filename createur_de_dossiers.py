9b82e0d1ebaf7d857f7f07b9b229410434781e6a
thomas <bezille.thomasb@gmail.com>
2024-11-12 16:28:10 +0100

    📦 NEW: Creation of second level folders

--- createur_de_dossiers.py
+++ createur_de_dossiers.py
@@ -21,5 +21,8 @@ folder_architecture = {
 }
 
 for folder in folder_architecture.keys():
-    folder_to_create = path / folder
-    folder_to_create.mkdir(exist_ok=True)
+    folder_to_create_first_level = path / folder
+    folder_to_create_first_level.mkdir(exist_ok=True)
+    for folder in folder_architecture[folder]:
+        folder_to_create_second_level = folder_to_create_first_level / folder
+        folder_to_create_second_level.mkdir(exist_ok=True)
