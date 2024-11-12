f4123276de41fa51fc0ca538a5a532e5502a81e5
thomas <bezille.thomasb@gmail.com>
2024-11-12 15:56:31 +0100

    📦 NEW: Creating variables that contain the path to the desired folder and folder architecture

---
+++ createur_de_dossiers.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+path = Path("/c/xampp/htdocs/DEV/PYTHON/formation_python_createur_de_dossier/dossier_test")
+folder_architecture = {
+    "Film": [
+        "Le seigneur des anneaux",
+        "Harry Potter",
+        "Moon",
+        "Forest Gump",
+    ],
+    "Employes": [
+        "Paul",
+        "Pierre",
+        "Marie",
+    ],
+    "Exercices": [
+        "les_variables",
+        "les_fichiers",
+        "les_boucles",
+    ]
+}
+
